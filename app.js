c47eb4460e87248a0ff40cfaf6998d36c201becd
trgomez <88842062+trrgomez@users.noreply.github.com>
2021-12-22 13:06:27 -0600

    copied style sheet using promises

--- app.js
+++ app.js
@@ -129,8 +129,19 @@ promptUser()
   .then(promptProject)
   .then((portfolioData) => {
     const pageHTML = generatePage(portfolioData);
-    fs.writeFile('./index.html', pageHTML, err => {
-      if (err) throw err;
-      console.log('Portfolio complete! Check out index.html to see the output!');
+    fs.writeFile('./dist/index.html', pageHTML, err => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log('Page created! Check out index.html in this directory to see it!');
+    
+      fs.copyFile('./src/style.css', './dist/style.css', err => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log('Style sheet copied successfully!');
+      });
     });
   });
